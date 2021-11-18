d2e2a54252b1e9b88c85741a1f369c86a35e403b
xihajun <www.junfan.com@gmail.com>
2021-11-18 12:03:03 +0000

    TODO check csv from drag function

---
+++ js/upload.js
@@ -0,0 +1,75 @@
+(function() {
+	"use strict";
+
+	var dropZone = document.getElementById('drop-zone');
+	var barFill = document.getElementById('bar-fill');
+	var barFillText = document.getElementById('bar-fill-text');
+	var uploadsFinished = document.getElementById('uploads-finished');
+
+	var startUpload = function (files) {
+
+//TODO check if it is csv
+    var file = files[0];
+    var reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = function(event) {
+      var csvData = event.target.result;
+      var data = $.csv.toArrays(csvData);
+      window.csv = data;
+
+      if (data && data.length > 0) {
+        displayHTMLTable();
+      } else {
+          alert('No data to import!');
+      }
+    };
+    reader.onerror = function() {
+        alert('Unable to read ' + file.fileName);
+    };
+
+	}
+
+	//standart form upload'
+	document.getElementById('standard-upload').addEventListener('click', function(e){
+	        var standardUploadFiles = document.getElementById('standard-upload-files').files;
+		e.preventDefault();
+		startUpload(standardUploadFiles);
+	});
+
+	//Drop functionality
+        dropZone.ondrop = function(e){
+	//TODO check if it is csv
+		e.preventDefault();
+		this.className="upload-console-drop";
+	        startUpload(e.dataTransfer.files);
+	};
+
+	dropZone.ondragover = function() {
+	 	this.className="upload-console-drop drop";
+		return false;
+	};
+
+	dropZone.ondragleave = function() {
+		this.className = "upload-console-drop";
+		return false;
+	};
+}());
+
+
+
+function arraysEqual(a, b) {
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length !== b.length) return false;
+
+    for (var i = 0; i < a.length; ++i) {
+      if (a[i] !== b[i]) return false;
+    }
+    return true;
+  }
+
+function displayHTMLTable() {
+
+    console.log(csv)
+    
+}
